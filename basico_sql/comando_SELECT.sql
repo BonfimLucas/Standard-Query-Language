5d240f33f5d208f1991458e259c16fd1a6a0fe00
Lucas <lucasbonfim604@gmail.com>
2022-12-25 15:03:07 -0300

    basico_sql

---
+++ basico_sql/comando_SELECT.sql
@@ -0,0 +1,19 @@
+select 
+  1,
+  2,
+  'ola',
+  "ola",
+  1 + 8,
+  2/10;
+
+  select
+    id,
+    created_at
+  from ecommerce.customers;
+
+   select
+    order_id,
+    quantity,
+    total_price
+  from ecommerce.items
+  limit 3;
