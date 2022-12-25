5b1a789149fe569a3f29b8b634fb1ff79a905c3e
Lucas <lucasbonfim604@gmail.com>
2022-12-25 16:09:11 -0300

    operadores

---
+++ basico_sql/operadores/operadores.sql
@@ -0,0 +1,31 @@
+# aritméticos
+select
+  1 + 1 as soma,
+  5 - 4 as subtracao,
+  7 * 2 as multiplicacao,
+  10 / 2 as divisao;
+
+# comparação
+select
+  1+1,
+  3 > 4,
+  4 > 4,
+  7 < 10,
+  11 <= 6;
+
+# between
+select
+  2 between 0 and 10,
+  -4 between 0 and 25;
+
+select
+  *
+from ecommerce.products
+where price between 10 and 50;
+
+# like
+select  	
+  *
+from ecommerce.customers
+where first_name like '%m%' or first_name like 'M%';
+
