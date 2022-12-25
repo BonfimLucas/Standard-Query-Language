a71680eed6f19ee0d790c2e1504510487aaf4b52
Lucas <lucasbonfim604@gmail.com>
2022-12-25 18:10:16 -0300

    operadores

--- basico_sql/operadores/operadores.sql
+++ basico_sql/operadores/operadores.sql
@@ -5,6 +5,7 @@ select
   7 * 2 as multiplicacao,
   10 / 2 as divisao;
 
+
 # comparação
 select
   1+1,
@@ -23,9 +24,33 @@ select
 from ecommerce.products
 where price between 10 and 50;
 
+
 # like
 select  	
   *
 from ecommerce.customers
 where first_name like '%m%' or first_name like 'M%';
+
+# in
+select
+  *
+from ecommerce.customers
+where state in ('Alagoas','Piauí');
+
+select
+  *
+from ecommerce.products
+where price in (11,15,70);
+
+# is null
+select
+  *
+from ecommerce.customers
+where email is null;
+
+# is not null
+select
+  *
+from ecommerce.customers
+where email is not null;
 
