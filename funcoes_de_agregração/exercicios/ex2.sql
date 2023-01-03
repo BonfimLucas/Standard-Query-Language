eec1f12bbe3d9b742c78f8c9c81a09d4d61ce542
Lucas <lucasbonfim604@gmail.com>
2023-01-02 21:18:05 -0300

    ex2

---
+++ funcoes_de_agregração/exercicios/ex2.sql
@@ -0,0 +1,14 @@
+#exercicio 2
+
+#Retorne a quantidade de pedidos de cada status por dia, ordenando pelo dia, de 
+#forma decrescente.
+
+select
+  count(id) as quantidade_de_pedidos,
+  format_timestamp("%D",created_at) as dia
+from ecommerce.orders
+group by dia
+order by dia desc
+
+
+
