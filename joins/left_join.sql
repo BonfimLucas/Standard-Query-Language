66402906a7fd5f6a85e56d8500b943ceba76fc23
Lucas <lucasbonfim604@gmail.com>
2023-01-09 21:57:39 -0300

    left join

---
+++ joins/left_join.sql
@@ -0,0 +1,18 @@
+select distinct
+  a.user_id as user_id_tabela_A,
+  c.user_id as user_id_tabelaC,
+  a.user_name,
+  c.price
+from banco_teste.acessos as a
+left join banco_teste.compras as c on a.user_id = c.user_id;
+
+#left join somente esquerda
+
+select distinct
+  a.user_id as user_id_tabela_A,
+  c.user_id as user_id_tabelaC,
+  a.user_name,
+  c.price
+from banco_teste.acessos as a
+left join banco_teste.compras as c on a.user_id = c.user_id
+where c.user_id is null
